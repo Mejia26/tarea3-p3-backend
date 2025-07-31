90f9dbc7378e777b494f34c07cc63857bfab0153
Mejia26 <112843498+Mejia26@users.noreply.github.com>
2025-07-30 21:26:55 -0400

    Revert "Validate hour range on save"

--- src/main/java/com/activityplanner/dto/ActivityDTO.java
+++ src/main/java/com/activityplanner/dto/ActivityDTO.java
@@ -3,22 +3,20 @@ package com.activityplanner.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ActivityDTO {
 
 	private UUID id;
-	
-	@NotNull(message="name cannot be null")
     private String name;
     private String icon;
     private String color;
--- src/main/java/com/activityplanner/util/ActivityValidator.java
+++
@@ -1,7 +0,0 @@
-package com.activityplanner.util;
-
-public class ActivityValidator {
-    public static boolean isHourInRange(Integer hour) {
-        return hour != null && hour >= 0 && hour <= 23;
-    }
-}
