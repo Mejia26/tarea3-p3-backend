f819662e66a67323d505ab72ca7e5c90672cbef9
Mejia26 <guidomejialiranzo26@gmail.com>
2025-07-30 21:22:27 -0400

    addes activity-dto validation

--- src/main/java/com/activityplanner/dto/ActivityDTO.java
+++ src/main/java/com/activityplanner/dto/ActivityDTO.java
@@ -3,20 +3,21 @@ package com.activityplanner.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
-@Builder
 public class ActivityDTO {
 
 	private UUID id;
+	
+	@NotNull(message="name cannot be null")
     private String name;
     private String icon;
     private String color;
